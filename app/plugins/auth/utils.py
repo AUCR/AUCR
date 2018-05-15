1fee33e33419711272edbc3178d30e6f2dbcc53c
Wyatt Roersma <wyattroersma@gmail.com>
2018-05-15 17:10:44 -0600

    Render reports and analysis view with user_groups_navbar

--- app/plugins/auth/utils.py
+++ app/plugins/auth/utils.py
@@ -1,6 +1,7 @@
 """AUCR auth plugin call utility library."""
 # coding=utf-8
 import glob
+import logging
 from flask import session
 from flask_login import current_user
 from app.plugins.auth.models import Group, Groups
@@ -35,15 +36,33 @@ def get_group_permission_navbar():
     """Return group nav list from database."""
     user_groups_ids["items"] = Group.query.filter_by(username=current_user.id).all()
     user_groups_links = {}
+    tasks_list = []
+    analysis_list = []
+    reports_list = []
     for items in user_groups_ids["items"]:
         group_object = Groups.query.filter_by(id=items.group_name).first()
+
         for filename in glob.iglob('app/plugins/**/navbar.yml', recursive=True):
             menu_links = YamlInfo(filename, "none", "none")
             run = menu_links.get()
             try:
-                if run["tasks"][group_object.group_name]:
-                    for items in run["tasks"][group_object.group_name]:
-                        user_groups_links["tasks"] = run["tasks"][group_object.group_name]
-            except:
+                if "tasks" in run:
+                    run_tasks_list = run["tasks"]
+                    for run_items in run_tasks_list[group_object.group_name]:
+                        tasks_list.append(run_items)
+                if "analysis" in run:
+                    run_analysis_list = run["analysis"]
+                    for analysis_items in run_analysis_list:
+                        analysis_list.append(analysis_items)
+                if "reports" in run:
+                    run_analysis_list = run["reports"]
+                    if run_analysis_list[group_object.group_name]:
+                        for report_items in run["reports"][group_object.group_name]:
+                            reports_list.append(report_items)
+            except KeyError as k:
+                logging.info("Key not found " + str(k))
                 pass
+    user_groups_links["tasks"] = tasks_list
+    user_groups_links["reports"] = reports_list
+    user_groups_links["analysis"] = analysis_list
     return user_groups_links
