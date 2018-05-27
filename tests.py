5544c6ab4039438b47d795cb4dac89547d440cf5
Wyatt Roersma <wyattroersma@gmail.com>
2018-05-27 04:12:35 -0600

    add back in some unit tests until I get time to fully convert to pytest tests

--- tests.py
+++ tests.py
@@ -57,3 +57,69 @@ def test_login(client):
 
 def test_logout(client):
     return client.get('/auth/logout', follow_redirects=True)
+
+
+
+csrf = CSRFProtect()
+
+
+@main_page.route('/test', methods=['GET'])
+@csrf.exempt
+def test_auth():
+    """Test group permissions."""
+    try:
+        if check_group("test"):
+            return "403"
+    except AttributeError:
+        return "403"
+
+
+class UserModelCase(unittest.TestCase):
+    """Unittests automated AUCR test case framework."""
+
+    def setUp(self):
+        """Set up needed base environment data for unittests."""
+        self.app = aucr_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        # Create a default sqlite database for testing
+        self.test_user_password = "0Qk9Bata3EO69U5T2qH57lAV1r67Wu"
+        test_user = User.__call__(username="test2", email="test2@example.com")
+        test_user.set_password(self.test_user_password)
+        db.session.add(test_user)
+        db.session.commit()
+        csrf.init_app(self.app)
+        self.csrf = CSRFProtect(self.app)
+        self.test_user = test_user
+
+    def tearDown(self):
+        """Destroy base environment data for unittests."""
+        db.session.remove()
+        # Drop the database
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_hashing(self):
+        """Test auth plugin password hashing."""
+        test_password_user = User.__call__(username="test1")
+        # These are randomly generated passwords for a longer complexity check to ensure this works as expected
+        test_password_user.set_password(self.test_user_password)
+        self.assertFalse(test_password_user.check_password('H2Y5Xns732tx27DyL509910dkJ8lSL'))
+        self.assertTrue(test_password_user.check_password(self.test_user_password))
+
+    def test_avatar(self):
+        """Test avatar creation for users."""
+        # Hard coded because we expect this to be the results
+        self.assertEqual(self.test_user.avatar(128),
+                         'https://www.gravatar.com/avatar/43b05f394d5611c54a1a9e8e20baee21?d=identicon&s=128')
+
+    def test_user_registration_and_auth(self):
+        """Test user auth plugin user registration."""
+        tests_errors_user = User.query.filter_by(username="test2").first()
+        if tests_errors_user is None or not tests_errors_user.check_password(self.test_user_password):
+            assert b'Invalid username or password'
+        self.assertTrue(tests_errors_user)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
