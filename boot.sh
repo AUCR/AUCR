1a163d55454d42e1c023769925d9e334d72c7750
Wyatt Roersma <wyatt.roersma@target.com>
2019-03-13 15:52:30 -0600

    update boot.sh to exec flask run for now until gunicorn/uwsgi can be properly implemented

--- boot.sh
+++ boot.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 # this script is used to boot a Docker container
-while true; do
-    flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Deploy command failed, retrying in 5 secs...
-    sleep 5
-done
-flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - aucr:app
+exec flask run --host=0.0.0.0
