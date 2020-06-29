b178b8dbdc2be6397ad80ce89b72cc0eda6fa834
Wyatt Roersma <wyattroersma@gmail.com>
2020-06-29 03:17:13 -0600

    Test no sql config

--- config.py
+++ config.py
@@ -9,12 +9,10 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     """Set environment variables based on config."""
-
+    #SQLALCHEMY_ENGINE_OPTIONS = {}
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'aucr.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_POOL_SIZE = os.environ.get('SQLALCHEMY_POOL_SIZE') or 20
-    SQLALCHEMY_MAX_OVERFLOW = os.environ.get('SQLALCHEMY_MAX_OVERFLOW') or 40
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SERVER_NAME = os.environ.get('SERVER_NAME')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
@@ -24,7 +22,10 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = os.environ.get("ADMIN_EMAILS") or ['admin@aucr.io']
     LANGUAGES = ['en']
+    #SQLALCHEMY_ENGINE_OPTIONS['pool_size'] = os.environ.get('SQLALCHEMY_POOL_SIZE') or None
+    #SQLALCHEMY_ENGINE_OPTIONS['max_overflow'] = os.environ.get('SQLALCHEMY_MAX_OVERFLOW') or None
     MAX_CONTENT_LENGTH = os.environ.get('MAX_CONTENT_LENGTH') or 2048 * 2048 * 2048
+    #SQLALCHEMY_ENGINE_OPTIONS['pool_timeout'] = os.environ.get('SQLALCHEMY_POOL_TIMEOUT') or None
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     FILE_FOLDER = os.environ.get('FILE_FOLDER') or os.path.join(basedir, 'upload')
@@ -34,7 +35,6 @@ class Config(object):
     ALLOWED_EXTENSIONS = os.environ.get('ALLOWED_EXTENSIONS') or ['txt', 'pdf', 'png', 'jpg',
                                                                   'jpeg', 'gif', 'doc', 'docx', 'exe', 'yar', 'zip', '']
     POSTS_PER_PAGE = os.environ.get('POSTS_PER_PAGE') or 25
-    SQLALCHEMY_POOL_TIMEOUT = os.environ.get('SQLALCHEMY_POOL_TIMEOUT') or None
     ALLOWED_EMAIL_LIST = (os.environ.get("ALLOWED_EMAIL_LIST") or "*").split(', ')
     LDAP_PROVIDER_URL = os.environ.get("LDAP_PROVIDER_URL") or None
     LDAP_PROTOCOL_VERSION = os.environ.get("LDAP_PROTOCOL_VERSION") or None
